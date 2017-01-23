783b53487906960f3f7ac2898344c2bb1056e913
Chris <orthagonal1@gmail.com>
2017-01-23 14:22:13 -0600

    test on travis

--- index.js
+++ index.js
@@ -19,7 +19,10 @@ module.exports = (commandName, options, callback) => {
   }
   // properties in options.env will over-ride the default process.env:
   if (options.env) {
+    console.log(options.env)
     options.env = Object.assign(process.env, options.env);
+    console.log('+')
+    console.log(options.env)
   } else {
     options.env = process.env;
   }
